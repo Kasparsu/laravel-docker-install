93642001dea6bd08031015fa9d448f56e96c6758
Kaspar Martin Suursalu <kasparsu@gmail.com>
2018-04-14 12:23:56 +0300

    add entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+if [ ! -d $PROJECT_NAME ]; then composer create-project --prefer-dist laravel/laravel $PROJECT_NAME; fi
+
+set -e
+
+# first arg is `-f` or `--some-option`
+if [ "${1#-}" != "$1" ]; then
+	set -- php-fpm "$@"
+fi
+
+exec "$@"
